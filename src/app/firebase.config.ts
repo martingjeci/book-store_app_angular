78920dc665f8d7e5ae285b9ba6507c9090e60b98
martingjeci <martingjeci@gmail.com>
2022-11-18 11:09:24 +0100

    Book Store Application

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCeH7AYCUIMCoKeSfImKEhFYKOZWuGlTeU",
+  authDomain: "angular1-course-72d1d.firebaseapp.com",
+  projectId: "angular1-course-72d1d",
+  storageBucket: "angular1-course-72d1d.appspot.com",
+  messagingSenderId: "1072388548159",
+  appId: "1:1072388548159:web:2c7e5cee4324ba4dddfa8a"
+};
